1a91512f14c6d59129e724923ef6524601afff16
EmanuelePalm <emanuele.palmieri@aesys.tech>
2021-08-30 16:54:55 +0200

    first commit

--- src/main/java/GRPCServer.java
+++ src/main/java/GRPCServer.java
@@ -1,18 +1,19 @@
 import contact.ContactService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-import it.palm.connection.Database;
 import user.UserService;
 
 public class GRPCServer {
 
 
     public static void main(String[] args) throws Exception {
+        System.out.println("Server starting");
         Server server = ServerBuilder.forPort(8080).addService( new UserService()).addService(new ContactService()).build();
 
         server.start();
         System.out.println("Server started at " + server.getPort());
         server.awaitTermination();
+        System.out.println("Termination");
 
     }
 }
